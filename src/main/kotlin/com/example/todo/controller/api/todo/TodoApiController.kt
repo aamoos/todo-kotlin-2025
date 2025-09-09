a1bddf45e58b33537f076b904c19dda35e102ba1
aamoos <aamoos@openmate.co.kr>
2025-09-09 10:58:03 +0900

    Service Logic 개발

--- src/main/kotlin/com/example/todo/controller/api/todo/TodoApiController.kt
+++ src/main/kotlin/com/example/todo/controller/api/todo/TodoApiController.kt
@@ -1,43 +1,60 @@
 package com.example.todo.controller.api.todo
 
 import com.example.todo.modl.http.TodoDto
+import com.example.todo.service.TodoService
 import jakarta.validation.Valid
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/todo")
-class TodoApiController {
+class TodoApiController(
+    val todoService: TodoService
+) {
 
     //R
     @GetMapping(path = [""])
-    fun read(@RequestParam(required = false) index:Int?){
+    fun read(@RequestParam(required = false) index:Int?): ResponseEntity<Any?> {
+        return index?.let {
+            todoService.read(it)
+        }?.let {
+            return ResponseEntity.ok(it)
+        }
+        ?: kotlin.run {
+            return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY)
+                .header(HttpHeaders.LOCATION, "/api/todo/all")
+                .build()
+        }
+    }
 
+    @GetMapping(path = ["/all"])
+    fun readAll(): MutableList<TodoDto> {
+        return todoService.readAll()
     }
 
     //C
     @PostMapping(path = [""])
-    fun create(@Valid @RequestBody todoDto: TodoDto){
-
+    fun create(@Valid @RequestBody todoDto: TodoDto): TodoDto? {
+        return todoService.create(todoDto)
     }
 
-    //U
+    //U TODO create = 201 내리도록, updte = 200 내리도록 변경
     @PutMapping(path = [""])
-    fun update(@Valid @RequestBody todoDto: TodoDto){
-
+    fun update(@Valid @RequestBody todoDto: TodoDto): TodoDto? {
+        return todoService.create(todoDto)
     }
 
     //D
     @DeleteMapping(path = ["/{index}"])
-    fun delete(@PathVariable(name = "index") _index:Int){
+    fun delete(@PathVariable(name = "index") _index:Int): ResponseEntity<Any>{
 
+        if(!todoService.delete(_index)){
+            return ResponseEntity.status(500).build()
+        }
+
+        return ResponseEntity.ok().build()
     }
 
 }
